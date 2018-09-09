5dac90eadca2ffa1512f812afa12dbe127b0e21c
Nusa <nesqic199@gmail.com>
2018-09-09 16:03:56 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+# render - ф-ция, которая соберет готовый шаблон для страницы
